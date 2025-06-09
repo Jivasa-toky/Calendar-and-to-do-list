b57755cb6757bccaffa74f4efd2af07c160fee30
Juvance <148179019+Jivasa-toky@users.noreply.github.com>
2025-06-09 09:32:57 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,7 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
 // Replace with your actual Firebase config
 const firebaseConfig = {
@@ -9,9 +13,9 @@ const firebaseConfig = {
   appId: "1:646469824544:web:05cb2e0fe46827bad8cc45"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// Firebase services
-const auth = firebase.auth();
-const db = firebase.firestore();
+export { auth, db };
+import{auth} from `./firebase-config.js`;
